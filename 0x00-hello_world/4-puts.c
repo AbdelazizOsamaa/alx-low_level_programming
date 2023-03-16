a20ff5188ff73ec32cce34d8fe00a546d44c2124
AbdelazizOsamaa <AbdelazizOsamaa@gmail.com>
2023-03-16 14:28:27 -0700

    main

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,6 @@
 
 /**
  * main - this is the main function
- *
  * Return: always 0
  */
 
@@ -11,6 +10,4 @@ int main(void)
 	puts("\"Programming is like building a multilingual puzzle");
 	
 	return (0);
-
-
 }
